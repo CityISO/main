2f16bc395896f7a9494327c31400e8d6b37e8ce1
Digitaljay <pushkareva_lisa@mail.ru>
2019-03-29 10:46:07 +0300

    Update cloud.py

--- cloud_parser/cloud.py
+++ cloud_parser/cloud.py
@@ -1,33 +1,34 @@
-d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
-
-# Read the whole text.
-text = open(path.join(d, 'somewords.txt')).read()
-
-# read the mask image
-# taken from
-# http://www.stencilry.org/stencils/movies/alice%20in%20wonderland/255fk.jpg
-alice_mask = np.array(Image.open(path.join(d, "logo.png")))
-
-#stopwords = set(STOPWORDS)
-#stopwords.add("said")
-
-wc = WordCloud(background_color="white", max_words=2000, mask=alice_mask,
-                contour_width=1, contour_color='white')
-
-# generate word cloud
-wc.generate(text)
-
-# store to file
-wc.to_file(path.join(d, "alice.png"))
-
-default_colors = wc.to_array()
-plt.title("Custom colors")
-plt.imshow(wc.recolor(colormap='bone', random_state=3),
-           interpolation="bilinear")
-# show
-plt.imshow(wc, interpolation='bilinear')
-plt.axis("off")
-#plt.figure()
-#plt.imshow(alice_mask, cmap=plt.cm.gray, interpolation='bilinear')
-# plt.axis("off")
-plt.show()
+def cloud(file):
+    import os
+    from os import path
+    from wordcloud import WordCloud
+    # get data directory (using getcwd() is needed to support running example in generated IPython notebook)
+    d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+    # Read the whole text.
+    #text = open(path.join(d,  file)).read()
+    f = open('TopicWordsCaption.txt', 'r')
+    text = [line.strip() for line in f]
+    f.close()
+    for i in range(0,len(text)//2,2):
+        a=text[i]
+        b=text[len(text)-i-1]
+        text[i]=b
+        text[len(text)-i-1]=a
+    # Generate a word cloud imageregexp=r'[^\r\n]+'
+    wordcloud = WordCloud(background_color='white',width=1600, height=800).generate(' '.join(text))
+    # Display the generated image:
+    # the matplotlib way:
+    import matplotlib.pyplot as plt
+    plt.imshow(wordcloud, interpolation='bilinear')
+    #     plt.axis("off")
+    # lower max_font_size
+    # wordcloud = WordCloud(max_font_size=40).generate(text)
+    # plt.figure()
+    # plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
+    plt.savefig('plt.png')
+    # The pil way (if you don't have matplotlib)
+    # image = wordcloud.to_image()
+    # image.show()
+cloud('TopicWordsCaption.txt')
