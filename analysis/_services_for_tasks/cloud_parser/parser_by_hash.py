941f71e3508d77364b649d04d4dc19e7d0664cd3
Digitaljay <pushkareva_lisa@mail.ru>
2019-03-29 17:08:23 +0300

    Parse by hash

--- analysis/_services_for_tasks/cloud_parser/parser_by_hash.py
+++ analysis/_services_for_tasks/cloud_parser/parser_by_hash.py
@@ -1,98 +1,79 @@
-def give_emoji_free_text(text):
-    import emoji
-    allchars = [str for str in text.decode('utf-8')]
-    emoji_list = [c for c in allchars if c in emoji.UNICODE_EMOJI]
-    clean_text = ' '.join([str for str in text.decode('utf-8').split() if not any(i in str for i in emoji_list)])
-    return clean_text
-def TopicDetectorCaption(caption):
-    text = caption
-    topics=[]
-    from rutermextract import TermExtractor
-    term_extractor = TermExtractor()
-    for term in term_extractor(text, limit = 3):
-        print(term.normalized)
-        print(term.count)
-        f_tc = open('TopicWordsCaption.txt', 'a')
-        f_tc.write(str(term.normalized) + " " + str(term.count) + + '\n')
-        f_tc.close()
-        topics.append(term.normalized)
-    return topics
-def commerc(haash):
-    from nltk.sentiment.vader import SentimentIntensityAnalyzer
-    from googletrans import Translator
-    import string
-
-    translator = Translator()
-    table=str.maketrans({key: None for key in string.punctuation})
-    sid = SentimentIntensityAnalyzer()
-
-    import pymorphy2
-    morph = pymorphy2.MorphAnalyzer()
+def image(hash_to_find):
     import instaloader
-    advert=["надевать","дизайн","вопрос","студия","покупка","предложение","скидка","новый","этаж","ссылка","каблук","заказ","продавцы","цена","размер","приобрести","информация","заказ","наращивание","запись","заказать","купить","стоимость","наличие","примерка","доставка","адрес","продажа","инструкция"]
-    L = instaloader.Instaloader(download_pictures=True, download_geotags=False, download_comments=False, download_videos=False, download_video_thumbnails=False, compress_json=False)
-    likes = instaloader.Post.get_likes(L)
-    EKB_id = 'old'
-    max_count = 5
-    ca=[]
+    import glob
+    import shutil
+    import os
+    import json
+    shutil.rmtree('test_hash')
+    #shutil.rmtree('#'+hash_to_find)
+
+    def main_color(file):
+            from PIL import Image
+            import colorsys
+            image = Image.open(file)
+            w, hh = image.size
+            ha = 0
+            for x in range(w):
+                for y in range(hh):
+                    r, g, b = image.getpixel((x, y))
+                    h,s,v=colorsys.hsv_to_rgb(r/256,g/256,b/256)
+                    ha += h
+            ha /= w * hh
+            gg = str(round(ha, 1))[2]
+            print(gg)
+            if int(gg)>7:
+                gg='7'
+            elif gg=='0':
+                gg='1'
+            shutil.copy(file, "test_hash\\" + gg)
+    newpath = 'test_hash'
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+    for i in range(1,8):
+        newpath = 'test_hash\\'+str(i)
+        if not os.path.exists(newpath):
+            os.makedirs(newpath)
+    L = instaloader.Instaloader(post_metadata_txt_pattern='', download_pictures=False, download_geotags=False, download_comments=False, download_videos=False, download_video_thumbnails=False, compress_json=False)
+    max_count = 300
     i = 0
-    hash_to_find=haash
     for post_EKB in L.get_hashtag_posts(hash_to_find):
         if post_EKB.likes > 15 and post_EKB.is_video == False and post_EKB.caption != "":
-            L.download_post(post_EKB, target=EKB_id)
+            L.download_post(post_EKB, target='#'+hash_to_find)
             User_Inf_List = [post_EKB.likes, post_EKB.comments, post_EKB.shortcode, post_EKB.owner_username] #Лайки, комменты, шорткод, никнейм
             f_Us_Inf = open('UserInf_' + str(i) +'.txt', 'w')
             for Us_inf in User_Inf_List:
                 f_Us_Inf.write(str(Us_inf) + '\n')
             f_Us_Inf.close()
-            i += 1
-            dec=0
-            unha=0
+            pic = glob.glob('#'+hash_to_find+"\\*.json")[0]
+            with open(pic, 'r') as f:
+                data = json.loads(f.read())
+                textt=(data['node']['thumbnail_resources'][0]['src'])
+            import urllib.request
+            url = textt
+            er = 'pict\\'+str(i)+'.jpeg'
+            urllib.request.urlretrieve(url, er)
             try:
-                for p in post_EKB.caption.split():
-                    if morph.parse(p.lower())[0].normal_form in advert:
-                        dec=1
-                    if ord(p[0])!=35:
-                        unha+=1
-            except(AttributeError):
-                dec=1
-            if dec==0 and unha!=0:
-                ca.append(post_EKB.caption)
+                main_color(er)
+            except:
+                pass
+            shutil.rmtree('#'+hash_to_find)
+            i += 1
             if i == max_count:
                 break
-    cap=ca
-    w=[]
-    charact=[]
-    pol=[]
-    otr=[]
-    neu=[]
-    for i in cap:
-        wt=0
-        t = give_emoji_free_text(i.encode('utf8'))
-        T=TopicDetectorCaption(t)
-        tt=t.translate(table)
-        for ww in tt.split():
-            if ww.isalpha()==False and ww!='\n':
-                wt=1
-        if wt!=1:
-            st=translator.translate(tt).text
-            ss=sid.polarity_scores(st)
-            sss=sorted(ss)
-            mark=float('{1}'.format(sss[0], ss[sss[0]]))
-            charact.append(mark)
-            print(t,T)
-            if mark>0:
-                for tT in T:
-                    pol.append(tT)
-            elif mark<0:
-                for tT in T:
-                    otr.append(tT)
-            else:
-                for tT in T:
-                    neu.append(tT)
-    print('compound: ', charact)
-    print('compound: ', sum(charact) / len(charact))
-    print(pol)
-    print(otr)
-    print(neu)
-commerc(input())
+    from PIL import Image
+    import glob
+    img = Image.new('RGB', (1650, 1650))
+    ars=[]
+    for i in range(8):
+        ars.append(glob.glob(f'test_hash\\{i+1}\\*.jpeg'))
+        print(ars)
+    for i in range(11):
+        for j in range(11):
+            print(ars[(i+j)//3])
+            im=Image.open(ars[(i+j)//3][0])
+            img.paste(im, (i*150,j*150))
+            if len(ars[(i+j)//3])>1:
+                ars[(i+j)//3].pop(0)
+    img.show()
+    img.save("out.jpg")
+image('staroměstskénáměstí')
