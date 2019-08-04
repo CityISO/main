c155d0ea4431981197a10f9cb6aba10be568eca8
mashed-potatoes <37037851+mashed-potatoes@users.noreply.github.com>
2019-08-04 19:47:07 +0500

    Hide E001

--- city_iso/settings.py
+++ city_iso/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+SILENCED_SYSTEM_CHECKS = ['mysql.E001']
+
 STATIC_URL = '/_static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '_static/')
 
